cf9a06ab397c632d52b4845025ab71594a0a216a
Ashutosh9713 <112890513+Ashutosh9713@users.noreply.github.com>
2025-11-05 15:46:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,65 @@
+// level 1
+
+// question 7
+// 7. Ask user’s age and check if eligible to vote
+// If age >= 18 → “Eligible”, else → “Not eligible”
+
+// let age = prompt("tell me your age ");
+
+// if(age>=18){
+//     console.log("you can vote")
+
+// }else{
+//     console.log("you can't vote")
+// }
+// -----------------------------------------
+
+// question 8
+// 8. Print multiplication table of 5
+// Use loop to print 5 × 1 to 5 × 10.
+
+// for(let i=1 ; i<=10 ; i++){
+//        console.log(`5*${i} is ${5*i}`);
+// }
+// ---------------------------------------------
+
+// question 9
+// 9. Count how many numbers between 1 and 15 are greater than 8
+// Loop and count conditionally.
+
+// let count  = 0 ;
+
+// for(let i = 1 ; i<=15 ; i++){
+//     if(i>8){
+//         count++
+//     }
+// }
+// console.log(count)
+// ----------------------------------------------------
+// question 10
+// 10. Ask user for password and print access status
+// Hardcoded correct password. Compare with user input.
+
+// let password = "bhai";
+// let userPass = prompt("tell me the password");
+// let attempt = 1;
+
+
+// if(userPass===null){
+//     console.log("password cannot be null")
+// }
+// while(userPass !== password){
+//     if(attempt>3){
+//         console.error("Account block")
+//         break;
+//     }
+//     console.log("wrong password")
+ 
+//     userPass = prompt("tell me the password");
+//     attempt++
+// }
+// if(userPass === password){
+//         console.log("correct password")
+// }
+// ---------------------------------------------
+
